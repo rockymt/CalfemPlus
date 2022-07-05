a6841babd3a97a2ba8b28fab3805cccc45db8c7d
rockymt <rockymt.liu@outlook.com>
2022-07-05 09:48:07 +0800

    Delete plant2BdToEl.m

--- plant2BdToEl.m
+++
@@ -1,8 +0,0 @@
-function [TEl] = plant2BdToEl(TBd,TEl)
-nbd=size(TBd,1);
-TEl=zeros(nbd,1);
-for i = 1:nbd
-   [i1 j1]=find(TEl(:,2:end)==TBd(i,1));
-   [i2 j2]=find(TEl(:,2:end)==TBd(i,2));
-   TEl(i)=intersect(i1,i2);
-end
